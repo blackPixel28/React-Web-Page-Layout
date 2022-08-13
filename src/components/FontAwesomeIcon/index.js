1e8b1a5b477179afdf09fff9723f9f902045caa7
Michal Snuszka <misiek28@gmail.com>
2022-08-13 20:17:12 +0200

    add icon NoPage, StartPage

--- src/components/FontAwesomeIcon/index.js
+++ src/components/FontAwesomeIcon/index.js
@@ -1,3 +1,3 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHome, faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons';
-library.add(faHome, faEnvelope, faFile)
+import { faHome, faFile, faEnvelope, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+library.add(faHome, faEnvelope, faFile, faTriangleExclamation)
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -2,16 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import '../css/Main.css'
 import StartPage from '../pages/StartPage';
 import PageLink from '../pages/PageLink';
+import NoPage from '../pages/NoPage';
 
 export default function Main() {
     return (
         <main className="app-main">
             <Routes>
-                <Route path="/" element={<StartPage />} />
+                <Route exact path="/" element={<StartPage />} />
                 <Route path="link1" element={<PageLink />} />
                 <Route path="link2" element={<PageLink />} />
                 <Route path="link3" element={<PageLink />} />
-                <Route path="link4" element={<PageLink />} />
+                <Route path="contact" element={<PageLink />} />
+                <Route path="*" element={<NoPage />} />
             </Routes>
         </main>
     )
