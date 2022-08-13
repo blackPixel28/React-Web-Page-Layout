12260bd405244f70149e7412737190420b7dae3b
Michal Snuszka <misiek28@gmail.com>
2022-08-13 13:29:19 +0200

    format code

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -19,7 +19,14 @@ function NavContener({ navDB }) {
 function NavLinks({ navDB }) {
     return (
         navDB.map((item, index) => (
-            <li className="nav-li"><Link key={`link-key:${index}`} to={navDB[index].link} className="nav-link">{navDB[index].text}</Link></li>
+            <li className="nav-li">
+                <Link
+                    key={`link-key:${index}`}
+                    to={navDB[index].link}
+                    className="nav-link">
+                    {navDB[index].text}
+                </Link>
+            </li>
         ))
     )
 }
