988148d87053cff26bbba8358126c2e95d425192
Michal Snuszka <misiek28@gmail.com>
2022-08-14 12:39:06 +0200

    fix min height page

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,23 @@ import Main from '../layout/Main';
 import Footer from '../layout/Footer';
 
 export default function App() {
+  const [headerH, setHeaderH] = useState(0);
+  const [footerH, setFooterH] = useState(0);
   const [mainH, setMainH] = useState(0);
-  useEffect(() => {
-    handleMainHeight()
-    document.querySelector(':root').style.setProperty('--mainHeight', `${mainH}px`)
-  })
 
-  const handleMainHeight = () => {
-    setMainH(window.innerHeight - document.querySelector('header').offsetHeight - document.querySelector('footer').offsetHeight)
+  useEffect(() => {
+    handleSetUseState()
+    window.addEventListener('orientationchange', () => handleSetUseState())
+    handleSetMainHeight()
+  })
+  const handleSetUseState = () => {
+    setHeaderH(document.querySelector('header').offsetHeight)
+    setFooterH(document.querySelector('footer').offsetHeight)
+    setMainH(window.screen.height)
+  }
+
+  const handleSetMainHeight = () => {
+    document.querySelector(':root').style.setProperty('--mainHeight', `${mainH - footerH - headerH}px`)
   }
   return (
     <div className="app">
