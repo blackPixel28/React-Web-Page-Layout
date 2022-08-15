9868a1efd09b9132c8cbf961580926ed7434e54b
Michal Snuszka <misiek28@gmail.com>
2022-08-15 19:16:00 +0200

    add button ArrowUp

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,9 +1,18 @@
-import '../css/Footer.css'
+import '../css/Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ArrowUp from '../components/ArrowUp'
 
 export default function Footer({ text }) {
     return (
         <footer className="app-footer">
-            <p>{text}</p>
+            <div className='footer-text'>
+                <p>{text}</p>
+                <p>{text}</p>
+                <p>{text}</p>
+                <p>{text}</p>
+                <p>{text}</p>
+            </div>
+            <FontAwesomeIcon className='footer-arrowUp' icon="fa-solid fa-circle-arrow-up" onClick={ArrowUp} />
         </footer>
     )
 }
