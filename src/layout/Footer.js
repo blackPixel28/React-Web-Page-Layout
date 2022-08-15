356f139e425d3957d499910fc02183febdf773ec
Michal Snuszka <misiek28@gmail.com>
2022-08-15 21:52:46 +0200

    arrowUP opactity

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,18 +1,46 @@
+import { useState, useEffect } from 'react';
 import '../css/Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import ArrowUp from '../components/ArrowUp'
+import ArrowUp from '../components/ArrowUp';
+import setCssRootValue from '../components/setCssRootValue';
 
 export default function Footer({ text }) {
+    const [arrowOpacity, setArrowOpacity] = useState(0);
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            handleActive(document.querySelector('.footer-arrowUp'))
+            // setCssArrowOpacity()
+            setCssRootValue('--arrowUpOpacity', arrowOpacity, '%')
+        })
+    })
+
+    const handleActive = (item) => {
+        const footerHeight = document.querySelector('footer').offsetHeight;
+        const maxScroll = document.body.scrollHeight - window.scrollY - window.innerHeight;
+        const opacity = Math.floor(((footerHeight * 100) / maxScroll) - 100);
+
+        // console.clear();
+        // console.log(maxScroll, opacity)
+
+        if (maxScroll <= (footerHeight + (footerHeight / 2))) {
+            if (opacity >= 0) setArrowOpacity(opacity);
+            else if (opacity >= 100) setArrowOpacity(100);
+            item.classList.add('active');
+            item.classList.remove('inactive');
+        }
+        else if (maxScroll >= (footerHeight + (footerHeight / 2))) {
+            item.classList.add('inactive');
+            item.classList.remove('active');
+        }
+    }
     return (
         <footer className="app-footer">
             <div className='footer-text'>
                 <p>{text}</p>
                 <p>{text}</p>
                 <p>{text}</p>
-                <p>{text}</p>
-                <p>{text}</p>
             </div>
-            <FontAwesomeIcon className='footer-arrowUp' icon="fa-solid fa-circle-arrow-up" onClick={ArrowUp} />
+            <FontAwesomeIcon className='footer-arrowUp inactive' icon="fa-solid fa-circle-arrow-up" onClick={ArrowUp} />
         </footer>
     )
 }
