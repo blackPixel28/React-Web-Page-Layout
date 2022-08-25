195f7e2433d03230f879fcaafac13168a3618178
Michal Snuszka <misiek28@gmail.com>
2022-08-25 16:51:18 +0200

    version 2 NoPage

--- src/pages/NoPage/NoPage.js
+++ src/pages/NoPage/NoPage.js
@@ -1,3 +1,4 @@
+import './css/index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function NoPage({ title, text }) {
@@ -8,16 +9,18 @@ export default function NoPage({ title, text }) {
 
     return (
         <section className="no-page">
-            <div className="content">
-                <h1>
-                    {' ' + title + ' '}
-                </h1>
-                <div>
-                    <FontAwesomeIcon icon="fa-solid fa-triangle-exclamation" color='#fcad03' fade className='trinagle' />
-                    <p className='no-page-text'>
-                        {text}
-                    </p>
-                    <button className='backBtn' onClick={handleBackPage}>WSTECZ</button>
+            <div className='wrap-content'>
+                <div className="content">
+                    <h1>
+                        {' ' + title + ' '}
+                    </h1>
+                    <div>
+                        <FontAwesomeIcon icon="fa-solid fa-triangle-exclamation" color='#fcad03' fade className='trinagle' />
+                        <p className='no-page-text'>
+                            {text}
+                        </p>
+                        <button className='backBtn' onClick={handleBackPage}>WSTECZ</button>
+                    </div>
                 </div>
             </div>
         </section>
