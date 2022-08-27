f42be54a9791448901ebca635c14d2b3e557cf06
Michal Snuszka <misiek28@gmail.com>
2022-08-27 18:42:45 +0200

    optimizing App

--- src/components/App.js
+++ src/components/App.js
@@ -4,43 +4,26 @@ import db from '../db/layout.json';
 import '../css/App.css'
 import '../css/media.css'
 import '../css/keyframes.css'
-
-import Header from '../layout/Header';
-import Main from '../layout/Main';
-import Footer from '../layout/Footer';
-import Nav from '../layout/Nav';
+import { Header, Main, Footer, Nav } from '../layout';
 
 export default function App() {
-  const [navH, setNavH] = useState(0);
-  const [headerH, setHeaderH] = useState(0);
-  const [footerH, setFooterH] = useState(0);
-  const [windowH, setWindowH] = useState(0);
-  const [mainH, setMainH] = useState(0);
+  const [stick, setStick] = useState(false);
 
   useEffect(() => {
-    handleResizeWindow()
-    window.addEventListener('orientationchange', () => window.location.reload())
-    window.addEventListener('resize', () => handleResizeWindow())
-    window.addEventListener('scroll', handleNavOffsetTop)
+    const headerHeight = document.querySelector('header').offsetHeight;
+    window.addEventListener('scroll', () => {
+      window.scrollY >= headerHeight ?
+        setStick(true) :
+        setStick(false);
+    })
+
+    return handleStick(stick)
   })
 
-  const handleResizeWindow = () => {
-    if (document.querySelector('.app').offsetHeight < window.innerHeight) document.querySelector(':root').style.setProperty('--mainHeight', `${windowH - headerH - footerH - navH - 1}px`);
-
-    setHeaderH(document.querySelector('header').offsetHeight)
-    setNavH(document.querySelector('nav').offsetHeight);
-    setFooterH(document.querySelector('footer').offsetHeight)
-    setWindowH(window.innerHeight);
-    setMainH(windowH - (headerH + footerH + navH) - 1);
-
-    document.querySelector(':root').style.setProperty('--mainHeight', `${mainH}px`)
-  }
-
-  const handleNavOffsetTop = () => {
+  const handleStick = () => {
     const nav = document.querySelector('nav');
-    nav.offsetTop > headerH ?
-      nav.classList.add('stick') :
-      nav.classList.remove('stick');
+
+    return stick === true ? nav.classList.add('stick') : nav.classList.remove('stick');
   }
 
   return (
