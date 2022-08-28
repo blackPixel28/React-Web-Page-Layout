52857de7cc8c499e245e61f945296e58fcaeb641
Michal Snuszka <misiek28@gmail.com>
2022-08-28 10:25:38 +0200

    mod component

--- src/components/getCssRoot/index.js
+++ src/components/getCssRoot/index.js
@@ -1,6 +1,6 @@
-const getCssRoot = (valueName = null) => {
+const getCssRoot = (valueName = null, className = ':root') => {
     if (valueName === null) return console.log('valueName is null')
-    return getComputedStyle(document.querySelector(':root')).getPropertyValue(`--${valueName}`)
+    return getComputedStyle(document.querySelector(className)).getPropertyValue(`--${valueName}`)
 }
 
 export default getCssRoot;
