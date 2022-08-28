3b56bbe4c47ee9b27c6e0cf4c596a12520a5e21f
Michal Snuszka <misiek28@gmail.com>
2022-08-28 20:50:03 +0200

    change theme switch

--- src/components/FontAwesomeIcon/index.js
+++ src/components/FontAwesomeIcon/index.js
@@ -1,3 +1,3 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHome, faFile, faEnvelope, faTriangleExclamation, faCircleArrowUp, faCircleArrowDown, faCircleArrowLeft, faCircleArrowRight, faCopyright, faFont } from '@fortawesome/free-solid-svg-icons';
-library.add(faHome, faEnvelope, faFile, faTriangleExclamation, faCircleArrowUp, faCircleArrowDown, faCircleArrowLeft, faCircleArrowRight, faCopyright, faFont)
+import { faHome, faFile, faEnvelope, faTriangleExclamation, faCircleArrowUp, faCircleArrowDown, faCircleArrowLeft, faCircleArrowRight, faCopyright, faFont, faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons';
+library.add(faHome, faEnvelope, faFile, faTriangleExclamation, faCircleArrowUp, faCircleArrowDown, faCircleArrowLeft, faCircleArrowRight, faCopyright, faFont, faCircleHalfStroke)
--- src/components/ThemeSwitch/index.js
+++ src/components/ThemeSwitch/index.js
@@ -1,16 +1,29 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState, useEffect } from 'react';
 import './css/index.css'
 
 const ThemeSwitch = ({ setActiveTheme, themes }) => {
+    const [theme, setTheme] = useState(false);
+    const [switchColor, setSwitchColor] = useState(false);
     const { light, dark } = themes;
+
+    useEffect(() => {
+        handleSwitchColor(theme);
+    })
+
+    const handleSwitchTheme = (boo) => {
+        setTheme(!boo);
+        return boo === false ? setActiveTheme(dark) : setActiveTheme(light);
+    }
+
+    const handleSwitchColor = (boo) => {
+        return boo === false ? setSwitchColor('white') : setSwitchColor('black');
+    }
 
     return (
         <div className="themeSwitch">
-            <div className='themeLight' onClick={() => setActiveTheme(light)}>
-                <FontAwesomeIcon icon="fa-solid fa-font" />
-            </div>
-            <div className='themeDark' onClick={() => setActiveTheme(dark)}>
-                <FontAwesomeIcon icon="fa-solid fa-font" />
+            <div className='switch' onClick={() => handleSwitchTheme(theme)}>
+                <FontAwesomeIcon icon="fa-solid fa-circle-half-stroke" color={switchColor} />
             </div>
         </div>
     )
